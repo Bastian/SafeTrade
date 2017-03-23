37c0ead32793a4193f2cc28191bb669139d42383
BtoBastian <bastianoppermann1997@gmail.com>
2017-03-23 19:53:14 +0100

    Fixed the InventoryClickListener

--- src/main/java/de/oppermann/bastian/safetrade/listener/InventoryClickListener.java
+++ src/main/java/de/oppermann/bastian/safetrade/listener/InventoryClickListener.java
@@ -34,128 +34,131 @@ public class InventoryClickListener implements Listener {
         if (trade == null) {
             return;
         }
-        final Inventory partnerInventory = trade.getInventoryOfPartner(event.getWhoClicked().getUniqueId());
-        if (trade.getCurrentAllowedSlots(event.getWhoClicked().getUniqueId()).contains(event.getRawSlot())) {
-            if (trade.isReadyOrHasAccepted(event.getWhoClicked().getUniqueId())) {
-                event.setCancelled(true);
-                return;
-            }
-            boolean checkedAction = false;
-            if (event.getAction() == InventoryAction.PICKUP_ALL) {
-                partnerInventory.setItem(event.getRawSlot() + 5, null);
-                checkedAction = true;
-            }
-            if (event.getAction() == InventoryAction.PICKUP_ONE) {
-                ItemStack toSet = getClickedInventory(event).getItem(event.getRawSlot()).clone();
-                toSet.setAmount(toSet.getAmount() - 1);
-                if (toSet.getAmount() == 0) {
-                    toSet = null;
+        if (getClickedInventory(event) == event.getWhoClicked().getOpenInventory().getTopInventory()) {
+            final Inventory partnerInventory = trade.getInventoryOfPartner(event.getWhoClicked().getUniqueId());
+            if (trade.getCurrentAllowedSlots(event.getWhoClicked().getUniqueId()).contains(event.getRawSlot())) {
+                if (trade.isReadyOrHasAccepted(event.getWhoClicked().getUniqueId())) {
+                    event.setCancelled(true);
+                    return;
                 }
-                partnerInventory.setItem(event.getRawSlot() + 5, toSet);
-                checkedAction = true;
-            }
-            if (event.getAction() == InventoryAction.PICKUP_HALF) {
-                ItemStack toSet = getClickedInventory(event).getItem(event.getRawSlot()).clone();
-                toSet.setAmount(toSet.getAmount() - (int) ((double) toSet.getAmount() / (double) 2 + 0.5));
-                if (toSet.getAmount() == 0) {
-                    toSet = null;
+                boolean checkedAction = false;
+                if (event.getAction() == InventoryAction.PICKUP_ALL) {
+                    partnerInventory.setItem(event.getRawSlot() + 5, null);
+                    checkedAction = true;
                 }
-                partnerInventory.setItem(event.getRawSlot() + 5, toSet);
-                checkedAction = true;
-            }
-            if (event.getAction() == InventoryAction.DROP_ONE_SLOT) {
-                ItemStack current = getClickedInventory(event).getItem(event.getRawSlot());
-                ItemStack toSet = current.clone();
-                toSet.setAmount(toSet.getAmount() - 1);
-                if (toSet.getAmount() == 0) {
-                    toSet = null;
+                if (event.getAction() == InventoryAction.PICKUP_ONE) {
+                    ItemStack toSet = getClickedInventory(event).getItem(event.getRawSlot()).clone();
+                    toSet.setAmount(toSet.getAmount() - 1);
+                    if (toSet.getAmount() == 0) {
+                        toSet = null;
+                    }
+                    partnerInventory.setItem(event.getRawSlot() + 5, toSet);
+                    checkedAction = true;
                 }
-                partnerInventory.setItem(event.getRawSlot() + 5, toSet);
-                checkedAction = true;
-            }
-            if (event.getAction() == InventoryAction.PLACE_ONE) {
-                ItemStack current = getClickedInventory(event).getItem(event.getRawSlot());
-                ItemStack toSet = event.getCursor().clone();
-                if (current != null && current.isSimilar(toSet)) {
-                    toSet.setAmount(current.getAmount() + 1);
-                } else {
-                    toSet.setAmount(1);
+                if (event.getAction() == InventoryAction.PICKUP_HALF) {
+                    ItemStack toSet = getClickedInventory(event).getItem(event.getRawSlot()).clone();
+                    toSet.setAmount(toSet.getAmount() - (int) ((double) toSet.getAmount() / (double) 2 + 0.5));
+                    if (toSet.getAmount() == 0) {
+                        toSet = null;
+                    }
+                    partnerInventory.setItem(event.getRawSlot() + 5, toSet);
+                    checkedAction = true;
                 }
-                partnerInventory.setItem(event.getRawSlot() + 5, toSet);
-                checkedAction = true;
-            }
-            if (event.getAction() == InventoryAction.PLACE_ALL) {
-                ItemStack current = getClickedInventory(event).getItem(event.getRawSlot());
-                ItemStack toSet = event.getCursor().clone();
-                if (current != null && current.isSimilar(toSet)) {
-                    toSet.setAmount(current.getAmount() + toSet.getAmount());
+                if (event.getAction() == InventoryAction.DROP_ONE_SLOT) {
+                    ItemStack current = getClickedInventory(event).getItem(event.getRawSlot());
+                    ItemStack toSet = current.clone();
+                    toSet.setAmount(toSet.getAmount() - 1);
+                    if (toSet.getAmount() == 0) {
+                        toSet = null;
+                    }
+                    partnerInventory.setItem(event.getRawSlot() + 5, toSet);
+                    checkedAction = true;
                 }
-                partnerInventory.setItem(event.getRawSlot() + 5, toSet);
-                checkedAction = true;
-            }
-            if (event.getAction() == InventoryAction.PLACE_SOME) {
-                ItemStack current = getClickedInventory(event).getItem(event.getRawSlot());
-                ItemStack toSet = event.getCursor().clone();
-                if (current != null && current.isSimilar(toSet)) {
-                    toSet.setAmount(current.getAmount() + toSet.getAmount());
+                if (event.getAction() == InventoryAction.PLACE_ONE) {
+                    ItemStack current = getClickedInventory(event).getItem(event.getRawSlot());
+                    ItemStack toSet = event.getCursor().clone();
+                    if (current != null && current.isSimilar(toSet)) {
+                        toSet.setAmount(current.getAmount() + 1);
+                    } else {
+                        toSet.setAmount(1);
+                    }
+                    partnerInventory.setItem(event.getRawSlot() + 5, toSet);
+                    checkedAction = true;
                 }
-                if (toSet.getAmount() > toSet.getMaxStackSize()) {
-                    toSet.setAmount(toSet.getMaxStackSize());
+                if (event.getAction() == InventoryAction.PLACE_ALL) {
+                    ItemStack current = getClickedInventory(event).getItem(event.getRawSlot());
+                    ItemStack toSet = event.getCursor().clone();
+                    if (current != null && current.isSimilar(toSet)) {
+                        toSet.setAmount(current.getAmount() + toSet.getAmount());
+                    }
+                    partnerInventory.setItem(event.getRawSlot() + 5, toSet);
+                    checkedAction = true;
                 }
-                partnerInventory.setItem(event.getRawSlot() + 5, toSet);
-                checkedAction = true;
-            }
-            if (event.getAction() == InventoryAction.SWAP_WITH_CURSOR) {
-                ItemStack toSet = event.getCursor().clone();
-                partnerInventory.setItem(event.getRawSlot() + 5, toSet);
-                checkedAction = true;
-            }
-            if (event.getAction() == InventoryAction.MOVE_TO_OTHER_INVENTORY) {
-                partnerInventory.setItem(event.getRawSlot() + 5, null);
-                checkedAction = true;
-            }
-            if (event.getAction() == InventoryAction.COLLECT_TO_CURSOR) {
-                event.setCancelled(true);
-                return;
-            }
-            if (!checkedAction) {
-                event.setCancelled(true);
-                return;
-            }
+                if (event.getAction() == InventoryAction.PLACE_SOME) {
+                    ItemStack current = getClickedInventory(event).getItem(event.getRawSlot());
+                    ItemStack toSet = event.getCursor().clone();
+                    if (current != null && current.isSimilar(toSet)) {
+                        toSet.setAmount(current.getAmount() + toSet.getAmount());
+                    }
+                    if (toSet.getAmount() > toSet.getMaxStackSize()) {
+                        toSet.setAmount(toSet.getMaxStackSize());
+                    }
+                    partnerInventory.setItem(event.getRawSlot() + 5, toSet);
+                    checkedAction = true;
+                }
+                if (event.getAction() == InventoryAction.SWAP_WITH_CURSOR) {
+                    ItemStack toSet = event.getCursor().clone();
+                    partnerInventory.setItem(event.getRawSlot() + 5, toSet);
+                    checkedAction = true;
+                }
+                if (event.getAction() == InventoryAction.MOVE_TO_OTHER_INVENTORY) {
+                    partnerInventory.setItem(event.getRawSlot() + 5, null);
+                    checkedAction = true;
+                }
+                if (event.getAction() == InventoryAction.COLLECT_TO_CURSOR) {
+                    event.setCancelled(true);
+                    return;
+                }
+                if (!checkedAction) {
+                    event.setCancelled(true);
+                    return;
+                }
 
-            Bukkit.getScheduler().runTaskLater(Main.getInstance(), new Runnable() {
+                Bukkit.getScheduler().runTaskLater(Main.getInstance(), new Runnable() {
 
-                @Override
-                public void run() {
-                    for (HumanEntity player : partnerInventory.getViewers()) {
-                        ((Player) player).updateInventory();
+                    @Override
+                    public void run() {
+                        for (HumanEntity player : partnerInventory.getViewers()) {
+                            ((Player) player).updateInventory();
+                        }
+                    }
+                }, 1);
+            } else { // if they aren't the allowed slots
+                if (trade.getCurrentAcceptSlots(event.getWhoClicked().getUniqueId()).contains(event.getRawSlot())) {
+                    trade.approve((Player) event.getWhoClicked());
+                } else if (trade.getCurrentAbortSlots(event.getWhoClicked().getUniqueId()).contains(event.getRawSlot())) {
+                    trade.abort((Player) event.getWhoClicked());
+                }
+
+                for (byte i = 0; i < 4; i++) {
+                    if (trade.getCurrentIncreaseMoneySlot(event.getWhoClicked().getUniqueId(), i).contains(event.getRawSlot())) {
+                        trade.changeMoney(event.getWhoClicked().getUniqueId(), i, event.isLeftClick());
                     }
                 }
-            }, 1);
-        } else { // if they aren't the allowed slots
-            if (trade.getCurrentAcceptSlots(event.getWhoClicked().getUniqueId()).contains(event.getRawSlot())) {
-                trade.approve((Player) event.getWhoClicked());
-            } else if (trade.getCurrentAbortSlots(event.getWhoClicked().getUniqueId()).contains(event.getRawSlot())) {
-                trade.abort((Player) event.getWhoClicked());
+                event.setCancelled(true);
             }
-
-            for (byte i = 0; i < 4; i++) {
-                if (trade.getCurrentIncreaseMoneySlot(event.getWhoClicked().getUniqueId(), i).contains(event.getRawSlot())) {
-                    trade.changeMoney(event.getWhoClicked().getUniqueId(), i, event.isLeftClick());
-                }
+        } else {
+            if (event.getAction() != InventoryAction.PICKUP_ALL &&
+                    event.getAction() != InventoryAction.PICKUP_HALF &&
+                    event.getAction() != InventoryAction.PICKUP_SOME &&
+                    event.getAction() != InventoryAction.PICKUP_ONE &&
+                    event.getAction() != InventoryAction.PLACE_ALL &&
+                    event.getAction() != InventoryAction.PLACE_SOME &&
+                    event.getAction() != InventoryAction.PLACE_ONE &&
+                    event.getAction() != InventoryAction.DROP_ONE_SLOT &&
+                    event.getAction() != InventoryAction.SWAP_WITH_CURSOR) {
+                event.setCancelled(true);
             }
-            event.setCancelled(true);
-        }
-        if (event.getAction() != InventoryAction.PICKUP_ALL &&
-                event.getAction() != InventoryAction.PICKUP_HALF &&
-                event.getAction() != InventoryAction.PICKUP_SOME &&
-                event.getAction() != InventoryAction.PICKUP_ONE &&
-                event.getAction() != InventoryAction.PLACE_ALL &&
-                event.getAction() != InventoryAction.PLACE_SOME &&
-                event.getAction() != InventoryAction.PLACE_ONE &&
-                event.getAction() != InventoryAction.DROP_ONE_SLOT &&
-                event.getAction() != InventoryAction.SWAP_WITH_CURSOR) {
-            event.setCancelled(true);
         }
     }
 
